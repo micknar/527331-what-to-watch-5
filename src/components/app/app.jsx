429a6b217f252de7a7d5147426d1bec20d444eaa
micknar <>
2020-10-06 01:25:18 +0300

    Добавлена маршрутизация

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,43 @@
 import React from "react";
-import Main from "../main/main";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import PropTypes from "prop-types";
+import Main from "../main/main";
+import SignIn from "../sign-in/sign-in";
+import MyList from "../my-list/my-list";
+import Film from "../film/film";
+import AddReview from "../add-review/add-review";
+import Player from "../player/player";
 
 const App = (props) => {
   const {title, genre, releaseDate} = props;
 
   return (
-    <Main
-      title={title}
-      genre={genre}
-      releaseDate={releaseDate}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main
+            title={title}
+            genre={genre}
+            releaseDate={releaseDate}
+          />
+        </Route>
+        <Route exact path="/login">
+          <SignIn />
+        </Route>
+        <Route exact path="/mylist">
+          <MyList />
+        </Route>
+        <Route exact path="/films/:id">
+          <Film />
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReview />
+        </Route>
+        <Route exact path="/player/:id">
+          <Player />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
