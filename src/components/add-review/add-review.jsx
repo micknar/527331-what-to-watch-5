fe397996531c258e870848dbf4e221b5bea3a2f5
micknar <>
2020-10-06 01:16:22 +0300

    Исправлены атрибуты, eslint

--- src/components/add-review/add-review.jsx
+++ src/components/add-review/add-review.jsx
@@ -4,7 +4,7 @@ const AddReview = () => {
   return (
     <section className="movie-card movie-card--full">
       <div className="movie-card__header">
-         <div className="movie-card__bg">
+        <div className="movie-card__bg">
           <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
         </div>
 
@@ -47,19 +47,19 @@ const AddReview = () => {
           <div className="rating">
             <div className="rating__stars">
               <input className="rating__input" id="star-1" type="radio" name="rating" value="1"/>
-              <label className="rating__label" for="star-1">Rating 1</label>
+              <label className="rating__label" htmlFor="star-1">Rating 1</label>
 
               <input className="rating__input" id="star-2" type="radio" name="rating" value="2" />
-              <label className="rating__label" for="star-2">Rating 2</label>
+              <label className="rating__label" htmlFor="star-2">Rating 2</label>
 
               <input className="rating__input" id="star-3" type="radio" name="rating" value="3" checked />
-              <label className="rating__label" for="star-3">Rating 3</label>
+              <label className="rating__label" htmlFor="star-3">Rating 3</label>
 
               <input className="rating__input" id="star-4" type="radio" name="rating" value="4" />
-              <label className="rating__label" for="star-4">Rating 4</label>
+              <label className="rating__label" htmlFor="star-4">Rating 4</label>
 
               <input className="rating__input" id="star-5" type="radio" name="rating" value="5" />
-              <label className="rating__label" for="star-5">Rating 5</label>
+              <label className="rating__label" htmlFor="star-5">Rating 5</label>
             </div>
           </div>
 
