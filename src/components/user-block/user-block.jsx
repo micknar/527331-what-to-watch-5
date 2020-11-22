234391ee8ffda80f6db90cfd3dc654161464f8b2
micknar <>
2020-11-22 17:26:25 +0300

    eslint fix

--- src/components/user-block/user-block.jsx
+++ src/components/user-block/user-block.jsx
@@ -11,12 +11,12 @@ const UserBlock = (props) => {
     <div className="user-block">
       {
         authorizationStatus === AuthorizationStatus.AUTH
-        ? <div className="user-block__avatar">
-          <Link to={AppRoute.MY_LIST}>
-            <img src="/img/avatar.jpg" alt="User avatar" width="63" height="63" />
-          </Link>
-        </div>
-        : <Link to={AppRoute.LOGIN} className="user-block__link">Sign in</Link>
+          ? <div className="user-block__avatar">
+            <Link to={AppRoute.MY_LIST}>
+              <img src="/img/avatar.jpg" alt="User avatar" width="63" height="63" />
+            </Link>
+          </div>
+          : <Link to={AppRoute.LOGIN} className="user-block__link">Sign in</Link>
       }
     </div>
   );
--- src/hocs/with-fullscreen-player/with-fullscreen-player.jsx
+++ src/hocs/with-fullscreen-player/with-fullscreen-player.jsx
@@ -79,6 +79,7 @@ const withFullscreenPlayer = (Component) => {
       );
     }
   }
+
   return WithFullscreenPlayer;
 };
 
